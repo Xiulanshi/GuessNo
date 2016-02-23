bf0b3b12de20fc6c5d094f5b9c5f3f38385e1aaf
Xiulan Shi <xiulanshi2013@gmail.com>
2016-02-22 23:46:47 -0500

    Updated

---
+++ GuessNo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuessNo
+//
+//  Created by Xiulan Shi on 2/22/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GuessNo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GuessNo
+//
+//  Created by Xiulan Shi on 2/22/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
